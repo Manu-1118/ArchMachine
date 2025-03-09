7a76f900800a7c532017d8a7537173454c3dbcd2
Manu-1118 <zetassj78@gmail.com>
2025-03-09 16:57:42 -0600

    The project is added.

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+//funcion para ejecutar las demas funciones cuando se cargue el DOM
+document.addEventListener('DOMContentLoaded', function () {
+    //Funciones
+    alerta();
+})
+
+//CODE
+
+function alerta() {
+    alert('Se cargo el js con exito');
+}
