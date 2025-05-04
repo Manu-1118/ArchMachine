a4dca35154ea02b15df393dfcdb14a272c8b909e
Manu-1118 <zetassj78@gmail.com>
2025-05-03 21:34:21 -0600

    Diseño principal de la pagina (bugs)

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,6 @@
 //funcion para ejecutar las demas funciones cuando se cargue el DOM
 document.addEventListener('DOMContentLoaded', function () {
     //Funciones
-    alerta();
 })
 
 //CODE
-
-function alerta() {
-    alert('Se cargo el js con exito');
-}
